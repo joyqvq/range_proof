c0cd1ac1a8a79611905ab406723b35d4a05556ab
Joy Wang <108701016+joyqvq@users.noreply.github.com>
2024-02-26 18:24:43 -0500

    add bench

--- benches/range_proof.rs
+++ benches/range_proof.rs
@@ -9,19 +9,19 @@ mod range_proof_benches {
     use merlin::Transcript;
     use range_proof::{commitment_scheme, range_proof::RangeProof};
 
-    fn proof(c: &mut Criterion) {
+    fn proof_32(c: &mut Criterion) {
         let n = 32usize;
         // let n = 64usize;
 
         let (pk, _vk) = commitment_scheme::trusted_setup(4usize * n).unwrap();
         let z = Fr::from(100u8);
         let mut proof_transcript = Transcript::new(b"range_proof");
-        c.bench_function("Proving", move |b| {
+        c.bench_function("Proving 32 bits", move |b| {
             b.iter(|| RangeProof::prove(&pk, n, &z, &mut proof_transcript))
         });
     }
 
-    fn verify(c: &mut Criterion) {
+    fn verify_32(c: &mut Criterion) {
         let n = 32usize;
         // let n = 64usize;
         let (pk, vk) = commitment_scheme::trusted_setup(4usize * n).unwrap();
@@ -30,7 +30,30 @@ mod range_proof_benches {
         let mut proof_transcript = Transcript::new(b"range_proof");
         let proof = RangeProof::prove(&pk, n, &z, &mut proof_transcript);
 
-        c.bench_function("Verifying", move |b| {
+        c.bench_function("Verifying 32 bits", move |b| {
+            b.iter(|| RangeProof::verify(&proof, &vk, n, &mut verification_transcript))
+        });
+    }
+
+    fn proof_64(c: &mut Criterion) {
+        let n = 64usize;
+        let (pk, _vk) = commitment_scheme::trusted_setup(4usize * n).unwrap();
+        let z = Fr::from(100u8);
+        let mut proof_transcript = Transcript::new(b"range_proof");
+        c.bench_function("Proving 64 bits", move |b| {
+            b.iter(|| RangeProof::prove(&pk, n, &z, &mut proof_transcript))
+        });
+    }
+
+    fn verify_64(c: &mut Criterion) {
+        let n = 64usize;
+        let (pk, vk) = commitment_scheme::trusted_setup(4usize * n).unwrap();
+        let z = Fr::from(100u8);
+        let mut verification_transcript = Transcript::new(b"range_proof");
+        let mut proof_transcript = Transcript::new(b"range_proof");
+        let proof = RangeProof::prove(&pk, n, &z, &mut proof_transcript);
+
+        c.bench_function("Verifying 64 bits", move |b| {
             b.iter(|| RangeProof::verify(&proof, &vk, n, &mut verification_transcript))
         });
     }
@@ -38,7 +61,7 @@ mod range_proof_benches {
     criterion_group! {
         name = range_proof_benches;
         config = Criterion::default().sample_size(100);
-        targets = proof, verify,
+        targets = proof_32, verify_32, proof_64, verify_64
     }
 }
 
